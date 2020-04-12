47cc5067ac85e7ef14919b2eeb9e0b70286181e7
Peng Ding <pding.dp@gmail.com>
2020-04-12 04:59:17 -0700

    fix main module unsafe import, patch single machine mode, and optimize performance

--- compute_percentiles.py
+++ compute_percentiles.py
@@ -20,77 +20,86 @@ import signal
 
 import argparse
 from pathlib import Path # to get $HOME dir
-psr = argparse.ArgumentParser(description='inferencing on descriptors')
-psr.add_argument('--in',  default='in_dir')
-psr.add_argument('--out', default='top1.csv')
-psr.add_argument('--perc', default=1, type=int)
-psr.add_argument('--dask', default=f'{Path.home()}/scheduler.json')
-
-args=vars(psr.parse_args())
-print(args)
-
-logging.info('processing '+ args['in'])
-
-
-# get the list of files recursively from root dir
-l=[]
-for root, subdirs, files in os.walk(args['in']):
-        for f in files:
-                if f.endswith('.csv'):
-                        l.append(root + '/' + f)
-
-logging.info ("found {} files".format(str(len(l))))
-
-
-# dask.distributed setup
 from dask.distributed import Client
-c = Client(scheduler_file=args['dask']) # Connect this local process to remote workers
+import dask
 
-# extra args to set up headers
-kwargs = {'names' : [0, 1, 2],
-        'memory_map': True, #enable memory map to alleviate IO overhead
-        }
+if __name__ == '__main__': # for main module safe import
+    psr = argparse.ArgumentParser(description='inferencing on descriptors')
+    psr.add_argument('--in',  default='in_dir')
+    psr.add_argument('--out', default='top1.csv')
+    psr.add_argument('--perc', default=1, type=int)
+    psr.add_argument('--dask')
 
-# make l small for testing
-# l=l[0:20000]
+    args=vars(psr.parse_args())
+    print(args)
 
-# parallel read on csv files
-logging.info('reading csv files')
-df = dd.read_csv(l, **kwargs)
-logging.info("done reading {:,} csv files".format(len(l)))
+    # get the list of files recursively from root dir
+    logging.info('processing '+ args['in'])
+    l=[]
+    for root, subdirs, files in os.walk(args['in']):
+            for f in files:
+                    if f.endswith('.csv'):
+                            l.append(root + '/' + f)
+
+    logging.info ("found {} files".format(str(len(l))))
+    
+    # dask.distributed setup
+    dask_kwargs = {}
+    if args['dask'] is None: # run in single machine mode
+        dask_kwargs['processes'] = True
+        dask_kwargs['n_workers'] = 8
+        dask_kwargs['memory_limit'] = '20G'
+    else: # Connect this local process to remote workers
+        dask_kwargs['scheduler_file'] = args['dask']
+
+    with dask.config.set({'distributed.worker.use-file-locking': False}):
+        with Client(**dask_kwargs) as c:
+            logging.info(f'dask info: {c}')
+
+            # extra args to set up headers
+            kwargs = {'names' : [0, 1, 2],
+                    'memory_map': True, #enable memory map to alleviate IO overhead
+                    }
+
+            # make l small for testing
+            # l=l[0:20000]
+
+            # parallel read on csv files
+            logging.info('reading csv files')
+            df = dd.read_csv(l, **kwargs)
+            df = df.repartition(npartitions=1000).persist() # repartition to optimize memory performance
+            logging.info("done reading {:,} csv files".format(len(l)))
 
 
-#logging.info('turn it into a pandas dataframe')
-#df=df.compute()
-#logging.info("{:,} rows with {:,} elements".format(df.shape[0], df.shape[1]))
+            #logging.info('turn it into a pandas dataframe')
+            #df=df.compute()
+            #logging.info("{:,} rows with {:,} elements".format(df.shape[0], df.shape[1]))
 
 
-p=1-(args['perc']/100)
-#r=str(100-args['perc']) + '%'
-logging.info("computing percentiles on pandas dataframe using {}".format(p))
-#d=df.describe(percentiles=[p])
-d = df[1].quantile(q=p)
-logging.info("computed percentiles on pandas dataframe")
-#logging.info("cutoff score for top {}% {:,}".format(args['perc'], d.at[r,1]))
+            p=1-(args['perc']/100)
+            #r=str(100-args['perc']) + '%'
+            logging.info("computing percentiles on pandas dataframe using {}".format(p))
+            #d=df.describe(percentiles=[p])
+            d = df[1].quantile(q=p)
+            logging.info("computed percentiles on pandas dataframe")
+            #logging.info("cutoff score for top {}% {:,}".format(args['perc'], d.at[r,1]))
 
 
-logging.info("identifying top {}%".format(args['perc']))
-#r=str(100-args['perc']) + '%'
-#t = df[df[1] > d.at[r,1]]
-t = df[df[1] > d]
-t = t.compute()
-logging.info("identified {:,} compounds in the top {}%".format(t.shape[0], args['perc']))
+            logging.info("identifying top {}%".format(args['perc']))
+            #r=str(100-args['perc']) + '%'
+            #t = df[df[1] > d.at[r,1]]
+            t = df[df[1] > d]
+            t = t.compute()
+#             logging.info("identified {:,} compounds in the top {}%".format(t.shape[0], args['perc']))
 
 
-logging.info ('writing csv')
-t.to_csv(args['out'])
-logging.info('done writing csv')
+            logging.info ('writing csv')
+            t.to_csv(args['out'])
+            logging.info('done writing csv')
 
-c.close() # close the dask.distributed client
-
-# for some reason, it takes forever for the program
-# to exit, am assuming it garbage collection, but can't prove it yet.
-gc.set_threshold(0)
-os.kill(os.getpid(), signal.SIGTERM)
+#             # for some reason, it takes forever for the program
+#             # to exit, am assuming it garbage collection, but can't prove it yet.
+#             gc.set_threshold(0)
+#             os.kill(os.getpid(), signal.SIGTERM)
 
 
--- compute_percentiles.sh
+++ compute_percentiles.sh
@@ -8,22 +8,39 @@ arg1=$1
 # arg2 is the percent cutoff
 arg2=$2
 
-DASK=$HOME/scheduler.json
-if [ -f "$DASK" ]; then
-    echo "$DASK exist"
+CLUSTER=false
+
+if $CLUSTER; then
+    DASK=$HOME/scheduler.json
+    if [ -f "$DASK" ]; then
+        echo "$DASK exist"
+        for n in $(cat $arg1) ; do
+
+            i=$n
+            b=$(basename $n)
+            python compute_percentiles.py --in $i --out "$b".top"$arg2".csv --perc $arg2 --dask $DASK > "$b".percentiles.log 2>&1
+            tail -n +2 "$b".top"$arg2".csv | cut -f2,3 -d',' > "$b".top"$arg2".csv.1 ;
+            sort -k2,2 -t',' -nr "$b".top"$arg2".csv.1 > "$b".top"$arg2".csv.2
+            echo "name,dock" > "$b".top"$arg2".csv.3
+            cat "$b".top"$arg2".csv.2 >> "$b".top"$arg2".csv.3
+
+       done
+    else
+        echo "$DASK does not exist"
+        echo "check if dask-mpi is running, maybe run \"sbatch setting1.sbatch\""
+    fi
+else
+    echo "run in single machine mode"
     for n in $(cat $arg1) ; do
 
         i=$n
         b=$(basename $n)
-        python compute_percentiles.py --in $i --out "$b".top"$arg2".csv --perc $arg2 --dask $DASK > "$b".percentiles.log 2>&1
+        python compute_percentiles.py --in $i --out "$b".top"$arg2".csv --perc $arg2 > "$b".percentiles.log 2>&1
         tail -n +2 "$b".top"$arg2".csv | cut -f2,3 -d',' > "$b".top"$arg2".csv.1 ;
         sort -k2,2 -t',' -nr "$b".top"$arg2".csv.1 > "$b".top"$arg2".csv.2
         echo "name,dock" > "$b".top"$arg2".csv.3
         cat "$b".top"$arg2".csv.2 >> "$b".top"$arg2".csv.3
 
    done
-else
-    echo "$DASK does not exist"
-    echo "check if dask-mpi is running, maybe run \"sbatch setting1.sbatch\""
 fi
 
