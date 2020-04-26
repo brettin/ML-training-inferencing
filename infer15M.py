b051583f03800992727ad043503f00aef2eea287
brettin <brettin@.anl.gov>
2020-04-26 09:53:34 -0700

    added fix_df function, add logging

--- infer15M.py
+++ infer15M.py
@@ -11,6 +11,13 @@ from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler
 
+import logging
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+
 # hard code args for testing
 # args={}
 # args['in'] = '/projects/CVD_Research/datasets/15M/xcg.smi.desc.fix'
@@ -46,10 +53,50 @@ def auroc( y_true, y_pred ) :
 
 dependencies={'r2' : r2, 'tf_auc' : tf_auc, 'auroc' : auroc }
 
+''' The goal of fix_df is to modify the df to conform to a
+single data frame format for use in inferencing. The desired
+format is that the first column has a sample identifier, and
+the rest of the columns have feature values.'''
+
+def fix_df (df):
+    # deal with header/no header, assumes header=None was passed to csv_read()
+    if df.columns.dtype != np.int64:
+        print ("did you read the csv file with header==None?")
+        sys.exit("can not fix df, use header=None in call to read_csv")
+    # assuming header=None, check if a header was actually in the csv file
+    # we'll do this by checking the datatype of the 4th column
+    elif df.iloc[:,4].dtype == object:
+        print("looks like there was a header row in the file")
+        df.drop(index=0, inplace=True)
+        df.infer_objects()
+    # deal with the first two columns being some combination of smile and name
+    col_types={}
+    for n in range(3):
+        if df.iloc[:,n].dtype == object:
+            print ("column {} looks like text".format(n))
+            col_types[n]=object
+        elif df.iloc[:,n].dtype == np.float64:
+            print ("column {} looks like float64".format(n))
+            col_types[n]=np.float64
+        elif df.iloc[:,n].dtype == np.float32:
+            print ("column {} looks like float32".format(n))
+            col_types[n]=float32
+    if col_types[0] == object and col_types[1] == object:
+        if df.iloc[:,0].str.len().mean() < df.iloc[:,1].str.len().mean():
+            print ("using col 0 as sample identifier")
+            df.drop(columns=[1], inplace=True)
+        else:
+            print ("using col 1 as sample identifier")
+            df.drop(columns=[0], inplace=True)
+    return df
+
 
 # read the csv descriptor file
 # assumes there is a header
-df=pd.read_csv(args['in'])
+logging.info('reading {}'.format(args['in']))
+df=pd.read_csv(args['in'], engine='python', header=None)
+df=fix_df(df)
+
 cols=df.shape[1] - 1
 rows=df.shape[0]
 samples=np.empty([rows,cols],dtype='float32')
