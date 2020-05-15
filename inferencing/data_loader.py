1f9cc49c8a60198f6427a8a498c06ced6bea2b42
Harry Yoo <hsyoo@anl.gov>
2020-05-15 08:15:43 -0700

    use threaded queue for writing to file

--- inferencing/data_loader.py
+++ inferencing/data_loader.py
@@ -1,24 +1,41 @@
 import pandas as pd
 import keras
-from sklearn.preprocessing import StandardScaler
 import csv
-import functools
-import operator
+import sys
+import threading, queue
 
 
 class DataLoader(keras.utils.Sequence):
     def __init__(self, file_format=None, input_list=[], label_file=None):
         self.input_list = input_list
         self.file_format = file_format
-        self.label_file = open(label_file, 'w')
-        self.label_writer = csv.writer(self.label_file, lineterminator='\n')
-        # self.labels = []
+        self.first_called = False
+        self.label_file = label_file
+        self.labels_q = queue.Queue()
+        def writer():
+            with open(self.label_file, 'w') as f:
+                label_writer = csv.writer(f, lineterminator='\n')
+                for labels in iter(self.labels_q.get, None):
+                    label_writer.writerows(labels)
+        threading.Thread(target=writer).start()
 
     def __len__(self):
         return len(self.input_list)
 
     def __getitem__(self, idx):
-        return self.load_dataset(self.input_list[idx])
+        batch_x, batch_y = self.load_dataset(self.input_list[idx])
+
+        if idx > 0 or (idx == 0 and self.first_called == False):
+            labels = batch_y.values.tolist()
+            self.labels_q.put(labels)
+
+        if idx == 0 and self.first_called == True:
+            self.labels_q.put(None)
+
+        if idx == 0 and self.first_called == False:
+            self.first_called = True
+
+        return batch_x, batch_y
 
     def load_dataset(self, file_name):
         try:
@@ -31,31 +48,30 @@ class DataLoader(keras.utils.Sequence):
             batch_x = df.iloc[:, 2:1615]
             batch_y = df.iloc[:, 0:2]
 
-            # save label to file
-            labels = batch_y.values.tolist()
-            self.label_writer.writerows(labels)
-
             return batch_x, batch_y
         except:
             print("Unexpected error:", sys.exc_info()[0])
             print(f'error loading {file_name}')
 
-    # def get_labels(self):
-    #    return functools.reduce(operator.iconcat, self.labels, [])
-
     def close(self):
-        self.label_file.close()
+        self.labels_q.put(None)
 
 
 class PredictLogger(keras.callbacks.Callback):
     def __init__(self, pred_file=None):
-        self.pred_file = open(pred_file, 'w')
-        self.pred_writer = csv.writer(self.pred_file, lineterminator='\n')
+        self.pred_file = pred_file
+        self.outputs_q = queue.Queue()
+        def writer():
+            with open(self.pred_file, 'w') as f:
+                pred_writer = csv.writer(f, lineterminator='\n')
+                for outputs in iter(self.outputs_q.get, None):
+                    pred_writer.writerows(outputs[0])
+        threading.Thread(target=writer).start()
 
     def on_predict_batch_end(self, batch, logs):
-        pred = logs['outputs'][0]
-        self.pred_writer.writerows(pred)
+        pred = logs['outputs']
+        self.outputs_q.put(pred)
 
     def on_predict_end(self, logs):
-        self.pred_file.close()
+        self.outputs_q.put(None)
 
