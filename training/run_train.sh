baec437ab5823ffaf1429218709c8d3e2cd990cd
brettin <brettin@.anl.gov>
2020-08-10 06:40:29 -0700

    added noclobber to cp

--- training/run_train.sh
+++ training/run_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p DIR.$1
-/bin/cp $1 DIR.$1
+/bin/cp -n $1 DIR.$1
 /bin/cp reg_go2.py DIR.$1
 cd DIR.$1
 CUDA_VISIBLE_DEVICES=$2 python reg_go2.py --ep 600 --in $1 >& output.log 
