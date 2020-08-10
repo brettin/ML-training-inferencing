8a8252c28d6fb34b15a343e02b9c98b111bb03d8
brettin <brettin@.anl.gov>
2020-08-10 06:12:13 -0700

    format mean

--- inferencing/infer-qc2.py
+++ inferencing/infer-qc2.py
@@ -3,12 +3,12 @@ import sys
 arg1=sys.argv[1]
 df=pd.read_csv (arg1, header=None)
 d=df.describe()
-# print ('name\tcount\tmean\tstd\tmin\t50%\tmax')
-print ('{}\t{}\t{:.0f}\t{:.2f}\t{:.2f}\t{:.2f}\t{:.2f}'.format(arg1,
-    d.at['count',1],
-    d.at['mean',1],
-    d.at['std',1],
-    d.at['min',1],
-    d.at['50%',1],
-    d.at['max',1]))
+print ('name\tcount\tmean\tstd\tmin\t50%\tmax')
+print ('{}\t{}\t{:.2f}\t{:.2f}\t{:.2f}\t{:.2f}\t{:.2f}'.format(arg1,
+    d.at['count',2],
+    d.at['mean',2],
+    d.at['std',2],
+    d.at['min',2],
+    d.at['50%',2],
+    d.at['max',2]))
 
