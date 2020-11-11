b6538a5a7af973bf3b9a980b5d7fee7fbad2d443
brettin <brettin@.anl.gov>
2020-11-11 13:01:25 -0800

    updated for new run

--- inferencing/infer-qc2.sh
+++ inferencing/infer-qc2.sh
@@ -2,4 +2,4 @@
 # Get the dirctory where this script lives
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-for n in $(cat done) ; do cd $n ; for m in $(ls *.top2000) ; do printf "$n\t" ; python $DIR/infer-qc2.py $m ; done ; cd .. ; done
+for n in $(cat done) ; do pushd $n ; for m in $(ls *.top.csv) ; do printf "$n\t" ; python $DIR/infer-qc2.py $m ; done ; popd ; done
